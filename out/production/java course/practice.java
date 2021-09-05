753748644b01853a89d1cd1e462c0694abb55ceb
shah-zx <shahnawazsayyed456@gmail.com>
2021-09-05 22:39:43 +0530

    For loop program

---
+++ out/production/java course/practice.java
@@ -0,0 +1,10 @@
+// package out.production.java course;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("Here we go with our for loop");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
