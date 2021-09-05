cfca11b8fdefbdb2db7eee43485a7f2d6242161d
shah-zx <shahnawazsayyed456@gmail.com>
2021-09-05 22:44:11 +0530

    Updated the loop

--- out/production/java course/practice.java
+++ out/production/java course/practice.java
@@ -1,9 +1,9 @@
-// package out.production.java course;
+
 
 public class practice {
     public static void main(String[] args) {
         System.out.println("Here we go with our for loop");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i < 7; i++) {
             System.out.println(i);
         }
     }
