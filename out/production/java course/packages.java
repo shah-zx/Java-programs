c61913cede757d49663f4af8d1dedb7fd9809950
shah-zx <shahnawazsayyed456@gmail.com>
2021-09-10 18:20:49 +0530

    update

--- out/production/java course/packages.java
+++ out/production/java course/packages.java
@@ -1,7 +1,14 @@
+
+
+import java.util.*;  // Here we imported all the things from the java.util // package
+
 public class packages {
     public static void main(String[] args) {
-        System.out.println(
-            "Hey , this is our java program!"
-        );
+        System.out.println("Hey , this is changed now!");
+        Scanner mysc = new Scanner(System.in);
+        int result = mysc.nextInt();
+        System.out.println("This is my scanner input as " + result);
+
     }
+
 }
