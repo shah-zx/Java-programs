5eccd8eaea37bc13d0e9d33e70a7c178944de6b3
shah-zx <shahnawazsayyed456@gmail.com>
2022-03-06 23:52:50 +0530

    Done with for loop

---
+++ .history/Programs/forloop_20220306235208.java
@@ -0,0 +1,11 @@
+package programs;
+
+public class forloop {
+    public static void main(String[] args) {
+        System.out.println("Our for loop ");
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
