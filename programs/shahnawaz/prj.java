4bfc9695dbb3355070676da08e41af4d13ab94fe
shah-zx <shahnawazsayyed456@gmail.com>
2022-04-03 00:14:05 +0530

    Done with constructor overloading

--- programs/shahnawaz/prj.java
+++ programs/shahnawaz/prj.java
@@ -13,13 +13,24 @@ public class prj {
             // name = "default name";
             // }
 
-            public Mymain(String naam , int pass) {  // Passing  a parametre
+            public Mymain(String naam, int pass) { // Passing a parametre
                 id = pass;
                 name = naam;
             }
 
+            // We can do constructor over loading as well :
+
+            public Mymain(int roll, int hieght, int weight) { // Passing a parametre
+                r = roll;
+                h = hieght;
+                w = weight;
+            }
+
             private int id;
             private String name;
+            public int r;
+            public int h;
+            private int w;
 
             public String getName() {
                 return name;
@@ -37,13 +48,30 @@ public class prj {
                 return id;
             }
 
+            public int getr() {
+                return r;
+            }
+
+            public int geth() {
+                return h;
+            }
+
+            public int getw() {
+                return w;
+            }
+
         }
-        Mymain shahnawaz = new Mymain("Hamza" ,78);
+        // Mymain shahnawaz = new Mymain("Hamza", 78);
+        Mymain hamza = new Mymain(23, 45, 67);
         // shahnawaz.setid(1); // if no constraints are given then the constructor will
         // come into picture
         // shahnawaz.setName("Coder");
-        System.out.println(shahnawaz.getName());
-        System.out.println(shahnawaz.getId());
+        // System.out.println(shahnawaz.getName());
+        // System.out.println(shahnawaz.getId());
+
+        System.out.println(hamza.getr());
+        System.out.println(hamza.getw());
+        System.out.println(hamza.geth());
 
     }
 }
