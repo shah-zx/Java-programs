d4c4e6c445bd646cd6c2c5e8849d15807f7d71b1
shahnawaz sayyed <shahnawazsayyed456@gmail.com>
2022-09-28 17:49:28 +0530

    Added

---
+++ Tutorials/Iterat.java
@@ -0,0 +1,25 @@
+package Tutorials;
+import java.util.LinkedList;
+import java.util.*;
+// import java.util.Iterator;
+
+
+public class Iterat {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("Shahnawaz");
+        list.add("Hamza");
+        list.add("Saurabh");
+        list.add("Govind");
+        list.add("Pooja");
+        System.out.println();
+        Iterat app = new Iterat();
+        app.printList(list);
+    }
+    void printList(List<String> list) {
+     Iterator<String> it = list.iterator();  // Printing the names using the iterator // 
+     while(it.hasNext()) {
+         System.out.println(it.next());
+     }
+    }
+}
